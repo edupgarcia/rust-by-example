5ada891ca3f28ddc3aa9356dd1e63285653decc9
Eduardo Pereira Garcia <edupgarcia.ti@gmail.com>
2020-07-03 15:59:15 -0300

    Hello with Ferris crate as module.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-extern crate ferris_says;
-
-use ferris_says::say;
-use std::io::{ stdout, BufWriter };
-
-fn ferris() {
-    let out = b"Hello fellow Rustaceans!";
-    let width = 24;
-
-    let mut writer = BufWriter::new(stdout());
-    say(out, width, &mut writer).unwrap();
-}
+mod ferris;
 
 fn main() {
-//    println!("Hello, world!");
-    ferris();
+    // println!("Hello, world!");
+    ferris::main();
 }
